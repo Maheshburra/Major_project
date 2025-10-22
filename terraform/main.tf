9e39a7c5d62fff572512dcc6377d2f4c9cb8ffaa
Maheshburra <maheshburra1121@gmail.com>
2025-10-23 00:37:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,28 +1,37 @@
-# Specify Docker provider
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.0"
+    }
+  }
+}
+
+# Configure the Docker provider
 provider "docker" {
   host = "npipe:////.//pipe//docker_engine"
 }
 
-# Docker image resource (pull local image built by Jenkins)
+# Pull the local Docker image built by Jenkins
 resource "docker_image" "major_project_image" {
   name = "major_project:latest"
 }
 
-# Docker containers
+# Deploy multiple container instances using the same image
 resource "docker_container" "app_instance1" {
-  name  = "app_instance1"
-  image = docker_image.major_project_image.latest
+  name    = "app_instance1"
+  image   = docker_image.major_project_image.image_id
   restart = "unless-stopped"
 }
 
 resource "docker_container" "app_instance2" {
-  name  = "app_instance2"
-  image = docker_image.major_project_image.latest
+  name    = "app_instance2"
+  image   = docker_image.major_project_image.image_id
   restart = "unless-stopped"
 }
 
 resource "docker_container" "app_instance3" {
-  name  = "app_instance3"
-  image = docker_image.major_project_image.latest
+  name    = "app_instance3"
+  image   = docker_image.major_project_image.image_id
   restart = "unless-stopped"
 }
