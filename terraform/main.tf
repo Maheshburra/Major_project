daccf38c38d7324fa956b2c0d14ce4921eb3495a
Maheshburra <maheshburra1121@gmail.com>
2025-10-23 00:56:02 +0530

    Refactor Docker resources with port mapping and lifecycle
    Updated Docker image and container configurations to include port mappings and lifecycle management.

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,37 +1,46 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 3.0.0"
-    }
+resource "docker_image" "app_image" {
+  name         = "major_project:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "app_instance1" {
+  name  = "app_instance1"
+  image = docker_image.app_image.image_id
+
+  ports {
+    internal = 8000
+    external = 8001
+  }
+
+  lifecycle {
+    replace_triggered_by = [docker_image.app_image]
   }
 }
 
-# Configure the Docker provider
-provider "docker" {
-  host = "npipe:////.//pipe//docker_engine"
-}
-
-# Pull the local Docker image built by Jenkins
-resource "docker_image" "major_project_image" {
-  name = "major_project:latest"
-}
-
-# Deploy multiple container instances using the same image
-resource "docker_container" "app_instance1" {
-  name    = "app_instance1"
-  image   = docker_image.major_project_image.image_id
-  restart = "unless-stopped"
-}
-
 resource "docker_container" "app_instance2" {
-  name    = "app_instance2"
-  image   = docker_image.major_project_image.image_id
-  restart = "unless-stopped"
+  name  = "app_instance2"
+  image = docker_image.app_image.image_id
+
+  ports {
+    internal = 8000
+    external = 8002
+  }
+
+  lifecycle {
+    replace_triggered_by = [docker_image.app_image]
+  }
 }
 
 resource "docker_container" "app_instance3" {
-  name    = "app_instance3"
-  image   = docker_image.major_project_image.image_id
-  restart = "unless-stopped"
+  name  = "app_instance3"
+  image = docker_image.app_image.image_id
+
+  ports {
+    internal = 8000
+    external = 8003
+  }
+
+  lifecycle {
+    replace_triggered_by = [docker_image.app_image]
+  }
 }
